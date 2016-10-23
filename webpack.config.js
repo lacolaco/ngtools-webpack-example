1bd8565534e7a9382d82c8663d33ee0013424907
laco0416 <laco0416+github@gmail.com>
2016-10-23 19:05:22 +0900

    make dev/prod build mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,42 @@
 const webpack = require('webpack');
 
-module.exports = {
-    entry: {
-        main: './src/main.ts'
-    },
-    output: {
-        path: './dist',
-        filename: '[name].bundle.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.html'],
-    },
-    module: {
-        rules: [
-            { test: /\.html$/, loader: 'raw' },
-            { test: /\.css$/, loader: 'raw' },
-            { test: /\.ts/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] }
-        ]
-    },
-    devtool: '#source-map',
+module.exports = (envOptions) => {
+    envOptions = envOptions || {};
+    const config = {
+        entry: {
+            main: './src/main.ts'
+        },
+        output: {
+            path: './dist',
+            filename: '[name].bundle.js',
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.html'],
+        },
+        module: {
+            rules: [
+                { test: /\.html$/, loader: 'raw' },
+                { test: /\.css$/, loader: 'raw' },
+                { test: /\.ts/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] }
+            ]
+        },
+        devtool: '#source-map',
+    };
+    if (envOptions.MODE === 'prod') {
+        config.plugins = [
+            new webpack.optimize.UglifyJsPlugin({
+                beautify: false,
+                mangle: {
+                    screw_ie8: true,
+                    keep_fnames: true
+                },
+                compress: {
+                    warnings: false,
+                    screw_ie8: true
+                },
+                comments: false
+            }),
+        ];
+    }
+    return config;
 };
