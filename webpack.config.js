73ff2ef6aa65f8f894ecece0bb3e877ad3ccfe71
Attila Papai <papaiatis@gmail.com>
2017-02-24 23:07:24 +0100

    Add support for lazy loaded routes in dev build

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,14 @@ module.exports = (envOptions) => {
         ];
     } else {
         config.module.rules.push(
-            { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] }
+            {
+                test: /\.ts$/,
+                loaders: [
+                    'awesome-typescript-loader',
+                    'angular2-template-loader',
+                    'angular2-router-loader'
+                ]
+            }
         );
     }
     return config;
