664c2027f83288bb5054f85eabda465236bb914d
Attila Papai <papaiatis@gmail.com>
2017-02-24 21:32:47 +0100

    Updated dependencies and fixed build workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const AotPlugin = require('@ngtools/webpack').AotPlugin;
+const helpers = require('./helpers');
 
 module.exports = (envOptions) => {
     envOptions = envOptions || {};
@@ -16,8 +17,8 @@ module.exports = (envOptions) => {
         },
         module: {
             rules: [
-                { test: /\.html$/, loader: 'raw' },
-                { test: /\.css$/, loader: 'raw' },
+                { test: /\.html$/, loader: 'raw-loader' },
+                { test: /\.css$/, loader: 'raw-loader' },
             ]
         },
         devtool: '#source-map',
@@ -29,7 +30,7 @@ module.exports = (envOptions) => {
         config.plugins = [
             new AotPlugin({
                 tsConfigPath: './tsconfig.json',
-                entryModule: 'src/app/app.module#AppModule'
+                entryModule: helpers.root('src/app/app.module#AppModule')
             }),
             new webpack.optimize.UglifyJsPlugin({
                 beautify: false,
